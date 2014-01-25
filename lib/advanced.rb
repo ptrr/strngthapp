8e9e74656cfeb7961c18f1b6ecde96b989d0646b
Peter de Ruijter <peter@l2code.nl>
2014-01-25 12:52:22 +0100

    adjusts maillift starting weights.

--- lib/advanced.rb
+++ lib/advanced.rb
@@ -1,24 +1,24 @@
 module Advanced
   def week1
     [
+      [5, Strength.round_with_smallest_increase(max_lift * 0.65)],
       [5, Strength.round_with_smallest_increase(max_lift * 0.75)],
-      [5, Strength.round_with_smallest_increase(max_lift * 0.8)],
       [5, Strength.round_with_smallest_increase(max_lift * 0.85)]
     ]
   end
 
   def week2
     [
-      [3, Strength.round_with_smallest_increase(max_lift * 0.8)],
-      [3, Strength.round_with_smallest_increase(max_lift * 0.85)],
+      [3, Strength.round_with_smallest_increase(max_lift * 0.70)],
+      [3, Strength.round_with_smallest_increase(max_lift * 0.80)],
       [3, Strength.round_with_smallest_increase(max_lift * 0.90)]
     ]
   end
 
   def week3
     [
-      [5, Strength.round_with_smallest_increase(max_lift * 0.85)],
-      [3, Strength.round_with_smallest_increase(max_lift * 0.9)],
+      [5, Strength.round_with_smallest_increase(max_lift * 0.75)],
+      [3, Strength.round_with_smallest_increase(max_lift * 0.85)],
       [1, Strength.round_with_smallest_increase(max_lift * 0.95)]
     ]
   end
--- lib/starting.rb
+++ lib/starting.rb
@@ -1,24 +1,24 @@
 module Starting
   def week1
     [
-      [5, Strength.round_with_smallest_increase(max_lift * 0.7)],
-      [5, Strength.round_with_smallest_increase(max_lift * 0.75)],
-      [5, Strength.round_with_smallest_increase(max_lift * 0.8)]
+      [5, Strength.round_with_smallest_increase(max_lift * 0.60)],
+      [5, Strength.round_with_smallest_increase(max_lift * 0.70)],
+      [5, Strength.round_with_smallest_increase(max_lift * 0.80)]
     ]
   end
 
   def week2
     [
+      [3, Strength.round_with_smallest_increase(max_lift * 0.65)],
       [3, Strength.round_with_smallest_increase(max_lift * 0.75)],
-      [3, Strength.round_with_smallest_increase(max_lift * 0.8)],
       [3, Strength.round_with_smallest_increase(max_lift * 0.85)]
     ]
   end
 
   def week3
     [
-      [5, Strength.round_with_smallest_increase(max_lift * 0.8)],
-      [3, Strength.round_with_smallest_increase(max_lift * 0.85)],
+      [5, Strength.round_with_smallest_increase(max_lift * 0.70)],
+      [3, Strength.round_with_smallest_increase(max_lift * 0.8)],
       [1, Strength.round_with_smallest_increase(max_lift * 0.9)]
     ]
   end
